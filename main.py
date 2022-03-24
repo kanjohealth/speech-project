b889c081e943d9600fba9a8183b18be3a3146f15
stefan-kanjo <stefan@mykanjo.com>
2022-03-24 13:47:51 +0000

    attempt to only push to newwork branch - attempt 2

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 # in the end this will be the start of the pipeline, let's build subfolders for scripts for each element of the pipeline/
+from scipy.io import wavfile
+from os import listdir
+from os.path import isfile, join
 
 print("Hello team speech-project!")
 
-mp3 = read_mp3
+mypath = "./raw_samples/"
+onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+print(onlyfiles)
+
+# loop through the list of raw samples and read the wav file
+for wav_filename in onlyfiles:
+    samplerate, data = wavfile.read("./raw_samples/"+wav_filename)
+
+    print(samplerate)
+# data has two channels - plot both
+
+
+
+
+#mp3 = read_mp3
